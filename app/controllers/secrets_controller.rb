b9768a8150aaee504a8aac6ffa8e112f935be0ff
山崎孝洋 <tak_3_zaki3@yahoo.co.jp>
2014-07-24 01:58:21 +0900

    もう眠い

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,19 +1,20 @@
+require 'cryptutil'
+
 class SecretsController < ApplicationController
   def maker
   end
 
   def show
   	#テキスト入力
-	textline = params[:page][:text]
-	@passwd = params[:passform][:password]
-	flg = params[:code_flg]
-	@title = "encode"
-    if flg == 1 then
-	  @title = "decode"
-    end
-	@text = textline.crypt(@passwd)
+  	textline = params[:page][:text]
+    p "--debug #{textline}"
+  	@passwd = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(16).join
+    #
+    obj = CryptUtil.new
+    @comtent, @title = obj.encrypt(@passwd,textline)
+    p "--debug #{@comtent}"
   end
   def decode
-
+    @title = "decode"
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Secretweet
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+        config.autoload_paths += %W(#{config.root}/app/controllers/concerns)
     # config.i18n.default_locale = :de
   end
 end
